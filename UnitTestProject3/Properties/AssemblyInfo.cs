6b61a530a7c8dc0817f9d59c73fc41b45e23f01d
Steven <110969196+Steven-LeMoal@users.noreply.github.com>
2022-10-16 12:36:47 +0200

    Add files via upload

---
+++ UnitTestProject3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject3")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f6febdc-b0cf-4808-9a17-1eb96e460857")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject3/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ProjetPOO
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Score_MotABC_Retourne2()
+        {
+            string mot = "abc";
+            int result = Jeu.Add_Score(mot);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Contain_MotABC_RetourneFalse()
+        {
+            string mot = "abc";
+            Joueur steven = new Joueur("Steven");
+            bool result = steven.Contain(mot);
+            Assert.AreEqual(false, result);
+        }
+
+
+        [TestMethod]
+        public void TailleMot_MotAB_RetourneFalse()
+        {
+            string mot = "ab";
+            bool result = Jeu.tailleMot(mot);
+            Assert.AreEqual(false, result);
+        }
+
+
+        [TestMethod]
+        public void TestTime_RetourneFalse()
+        {
+            DateTime now1 = DateTime.Now;
+            int max = 10;//10 minutes
+            DateTime now2 = DateTime.Now;
+
+            bool result = Jeu.TestTime(now1, now2, max);
+            Assert.AreEqual(false,!result);
+        }
+
+        [TestMethod]
+        public void Add_Mot_MotABC_RetourneFalse()
+        {
+            string mot = "abc";
+            Joueur steven = new Joueur("Steven");
+            steven.Add_Mot(mot);
+            Assert.AreEqual("abc", steven.MotUsee[0]);
+
+        }
+    }
+}
